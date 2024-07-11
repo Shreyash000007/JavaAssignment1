d1563e01a97dbe2b7f84a2313f53d9ff97afd371
Shreyash000007 <shreyasharg@gmail>
2024-07-11 13:29:32 +0530

    ffff

---
+++ src/Team.java
@@ -0,0 +1,10 @@
+public class Team {
+    public void name(){
+        System.out.println("Shreyash");
+        System.out.println("Abhishek");
+        System.out.println("Astik");
+        System.out.println("Ratan");
+        System.out.println("Ram");
+    }
+    
+}
